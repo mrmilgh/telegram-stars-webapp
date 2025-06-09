000afe8c5a41c0f150be08424986d1c56a9aed43
Miladex <godoofwar123@gmail.com>
2025-06-09 19:02:51 +0330

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from api.stars import router as stars_router
+from vercel_fastapi import VercelFastAPI
 
 app = FastAPI()
-
 app.include_router(stars_router, prefix="/api")
+
+# Adapter برای Vercel
+handler = VercelFastAPI(app)
