4c337729fcd9b336d81cf08af143e9633f9a411e
Miladex <godoofwar123@gmail.com>
2025-06-09 21:17:52 +0330

    main.py

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ from mangum import Mangum
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 app = FastAPI()
 
+# اتصال API
 app.include_router(stars_router, prefix="/api")
 
+# مسیر فایل‌های استاتیک (frontend)
 frontend_path = os.path.join(BASE_DIR, "public")
-app.mount("/static", StaticFiles(directory=frontend_path, html=True), name="static")
+app.mount("/static", StaticFiles(directory=frontend_path), name="static")
 
+# سرو فایل index.html در مسیر root
 @app.get("/")
 def serve_index():
     return FileResponse(os.path.join(frontend_path, "index.html"))
 
+# هندلر Vercel
 handler = Mangum(app)
