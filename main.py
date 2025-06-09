09b70fa821d5f034637303eede57e4b90448fbb3
Miladex <godoofwar123@gmail.com>
2025-06-09 19:23:52 +0330

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import os
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from api.stars import router as stars_router
 from mangum import Mangum
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 app = FastAPI()
 
 app.include_router(stars_router, prefix="/api")
-app.mount("/static", StaticFiles(directory="frontend", html=True), name="static")
+
+frontend_path = os.path.join(BASE_DIR, "frontend")
+app.mount("/static", StaticFiles(directory=frontend_path, html=True), name="static")
 
 @app.get("/")
 def serve_index():
-    return FileResponse("frontend/index.html")
+    return FileResponse(os.path.join(frontend_path, "index.html"))
 
 handler = Mangum(app)
